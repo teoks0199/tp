e766c22f5ddc64236f61444d5594b660e0e48cd4
BEATRICE <beatricechen2003@gmail.com>
2023-10-14 21:46:06 +0800

    Edit test file

--- src/test/java/seedu/address/logic/commands/AddStallCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddStallCommandTest.java
@@ -95,11 +95,6 @@ public class AddStallCommandTest {
         }
 
         @Override
-        public void showStall(Stall stall) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
         public ReadOnlyUserPrefs getUserPrefs() {
             throw new AssertionError("This method should not be called.");
         }
