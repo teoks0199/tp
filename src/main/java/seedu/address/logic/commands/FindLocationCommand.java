d31f64a15af6a9d332b190945490eb7f0e304e74
teoks0199 <teokaisheng3@gmail.com>
2023-10-27 00:26:37 +0800

    Fix javadocs

--- src/main/java/seedu/address/logic/commands/FindLocationCommand.java
+++ src/main/java/seedu/address/logic/commands/FindLocationCommand.java
@@ -8,7 +8,7 @@ import seedu.address.model.Model;
 import seedu.address.model.stall.LocationContainsKeywordsPredicate;
 
 /**
- * Finds and lists all stalls in address book whose name contains any of the argument keywords.
+ * Finds and lists all stalls in address book whose location contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
 public class FindLocationCommand extends Command {
