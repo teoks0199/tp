17bef2f938b02444f14ba415456f1894312d053d
samuelmui8 <samuelmui8@gmail.com>
2023-10-29 02:01:16 +0800

    Add testcases

--- src/main/java/seedu/address/model/stall/StallPriceComparator.java
+++ src/main/java/seedu/address/model/stall/StallPriceComparator.java
@@ -8,6 +8,14 @@ import java.util.Comparator;
 public class StallPriceComparator implements Comparator<Stall> {
     @Override
     public int compare(Stall stall1, Stall stall2) {
+        if (stall1.getAveragePrice() == null && stall2.getAveragePrice() == null) {
+            return 0;
+        } else if (stall1.getAveragePrice() == null) {
+            return 1;
+        } else if (stall2.getAveragePrice() == null) {
+            return -1;
+        }
+
         return stall1.getAveragePrice().compareTo(stall2.getAveragePrice());
     }
 }
--- src/test/java/seedu/address/logic/commands/AddStallCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddStallCommandTest.java
@@ -251,6 +251,11 @@ public class AddStallCommandTest {
             throw new AssertionError("This method should not be called.");
         }
 
+        @Override
+        public void sortStallPrice() {
+            throw new AssertionError("This method should not be called.");
+        }
+
     }
 
     /**
