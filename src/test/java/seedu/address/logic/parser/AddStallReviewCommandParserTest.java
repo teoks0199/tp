d60dd52fbcd45fd7c4ef336002c9525fb5780d8c
jamesebond <e1032444@u.nus.edu>
2023-11-11 06:45:14 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/parser/AddStallReviewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddStallReviewCommandParserTest.java
@@ -1,9 +1,5 @@
 package seedu.address.logic.parser;
 
-
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.AddStallReviewCommand;
-
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_STALL_INDEX_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_STALL_RATING;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_STALL_DESC;
@@ -16,27 +12,30 @@ import static seedu.address.model.review.Rating.MESSAGE_CONSTRAINTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STALL;
 import static seedu.address.testutil.TypicalStallReviews.STALL_REVIEW_3;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.AddStallReviewCommand;
+
 public class AddStallReviewCommandParserTest {
 
     private AddStallReviewCommandParser parser = new AddStallReviewCommandParser();
+
     @Test
     public void parse_validInput_returnsAddStallReviewCommand() {
-       assertParseSuccess(parser,VALID_STALL_INDEX_DESC + VALID_STALL_RATING + VALID_STALL_DESC,
-               new AddStallReviewCommand(STALL_REVIEW_3, INDEX_FIRST_STALL));
+        assertParseSuccess(parser, VALID_STALL_INDEX_DESC + VALID_STALL_RATING + VALID_STALL_DESC,
+                new AddStallReviewCommand(STALL_REVIEW_3, INDEX_FIRST_STALL));
     }
 
-    //Invalid stall index
+    // Invalid stall index
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, INVALID_STALL_INDEX_DESC + VALID_STALL_RATING + VALID_STALL_DESC,
-               MESSAGE_INVALID_STALL_INDEX);
+                MESSAGE_INVALID_STALL_INDEX);
     }
 
-    //Invalid stall rating
+    // Invalid stall rating
     @Test
     public void parse_invalidArgs_throwsParseException2() {
         assertParseFailure(parser, VALID_STALL_INDEX_DESC + INVALID_STALL_RATING + VALID_STALL_DESC,
-               MESSAGE_CONSTRAINTS);
+                MESSAGE_CONSTRAINTS);
     }
-
 }
