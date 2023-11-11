6804caf552f63073c241ef399ea74922945a84b2
jamesebond <e1032444@u.nus.edu>
2023-11-11 10:37:19 +0800

    add test cases

--- src/test/java/seedu/address/logic/commands/AddStallReviewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddStallReviewCommandTest.java
@@ -1,9 +1,13 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STALL;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_STALL;
 import static seedu.address.testutil.TypicalStalls.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -63,5 +67,58 @@ public class AddStallReviewCommandTest {
 
         assertCommandFailure(command, model, Messages.MESSAGE_INVALID_STALL_DISPLAYED_INDEX);
     }
+
+    // Test equality with the same object
+    @Test
+    public void equals_sameObject_returnsTrue() {
+        StallReview stallReview = new StallReviewBuilder().build();
+        AddStallReviewCommand command = new AddStallReviewCommand(stallReview, INDEX_FIRST_STALL);
+        assertEquals(command, command);
+    }
+
+    // Test equality with objects with same values
+    @Test
+    public void equals_sameValues_returnsTrue() {
+        StallReview stallReview = new StallReviewBuilder().build();
+        AddStallReviewCommand command1 = new AddStallReviewCommand(stallReview, INDEX_FIRST_STALL);
+        AddStallReviewCommand command2 = new AddStallReviewCommand(stallReview, INDEX_FIRST_STALL);
+        assertEquals(command1, command2);
+    }
+
+    // Test non-equality with a different type of object
+    @Test
+    public void equals_differentTypes_returnsFalse() {
+        StallReview stallReview = new StallReviewBuilder().build();
+        AddStallReviewCommand command = new AddStallReviewCommand(stallReview, INDEX_FIRST_STALL);
+        assertFalse(command.equals(new Object()));
+    }
+
+    // Test non-equality with null
+    @Test
+    public void equals_null_returnsFalse() {
+        StallReview stallReview = new StallReviewBuilder().build();
+        AddStallReviewCommand command = new AddStallReviewCommand(stallReview, INDEX_FIRST_STALL);
+        assertFalse(command.equals(null));
+    }
+
+    // Test non-equality with different StallReview
+    @Test
+    public void equals_differentStallReview_returnsFalse() {
+        StallReview stallReview1 = new StallReviewBuilder().withRating("4").build();
+        StallReview stallReview2 = new StallReviewBuilder().withRating("5").build();
+        AddStallReviewCommand command1 = new AddStallReviewCommand(stallReview1, INDEX_FIRST_STALL);
+        AddStallReviewCommand command2 = new AddStallReviewCommand(stallReview2, INDEX_FIRST_STALL);
+        assertNotEquals(command1, command2);
+    }
+
+    // Test non-equality with different Index
+    @Test
+    public void equals_differentIndex_returnsFalse() {
+        StallReview stallReview = new StallReviewBuilder().build();
+        AddStallReviewCommand command1 = new AddStallReviewCommand(stallReview, INDEX_FIRST_STALL);
+        AddStallReviewCommand command2 = new AddStallReviewCommand(stallReview, INDEX_SECOND_STALL);
+        assertNotEquals(command1, command2);
+    }
+
 
 }
